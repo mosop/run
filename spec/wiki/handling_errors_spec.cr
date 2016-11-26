da160f67ac8ad27ac90f60abc5aa5cb707ddcf05
mosop <mosop@users.noreply.github.com>
2016-11-26 22:08:07 +0900

    test

--- spec/wiki/handling_errors_spec.cr
+++ spec/wiki/handling_errors_spec.cr
@@ -4,7 +4,7 @@ module RunWikiHandlingErrorsFeature
   module Abort
     describe name do
       it "parallel" do
-        s = Stdio.capture do |io|
+        Stdio.capture do |io|
           cg = Run::CommandGroup.new(abort_timeout: 5) do |g|
             g.command TRAP_SIGNAL
             g.command "fail", abort_on_error: true
@@ -12,12 +12,10 @@ module RunWikiHandlingErrorsFeature
           pg = cg.run(parallel: true)
           pg.wait
           p = pg[0]
-          io.out.gets_to_end
+          io.out.gets_to_end.should eq "15\n"
           # p.output.rewind
           # p.output.gets_to_end.should eq "15\n"
         end
-        puts "!!!"
-        puts s
       end
     end
   end
