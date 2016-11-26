61fc814459f72149f3196b085c33e1fe0a4cf395
mosop <mosop@users.noreply.github.com>
2016-11-26 21:26:19 +0900

    test

--- spec/wiki/handling_errors_spec.cr
+++ spec/wiki/handling_errors_spec.cr
@@ -2,26 +2,11 @@ require "../spec_helper"
 
 module RunWikiHandlingErrorsFeature
   module Abort
-    # cg = Run::CommandGroup.new do |g|
-    #   g.command "wget", %w(http://essential.com), abort_on_error: true
-    #   g.command "wget", %w(http://optional.com)
-    # end
-    # cg.run
     describe name do
-      it "serial" do
-        Stdio.capture do |io|
-          cg = Run::CommandGroup.new do |g|
-            g.command "fail", error: false, abort_on_error: true
-            g.command "echo", %w(test)
-          end
-          io.out.gets_to_end.should eq ""
-        end
-      end
-
       it "parallel" do
-        cg = Run::CommandGroup.new(abort_timeout: Run::Timeout::INFINITE) do |g|
-          g.command TRAP_SIGNAL, output: IO::Memory.new
-          g.command "fail", error: false, abort_on_error: true
+        cg = Run::CommandGroup.new(abort_timeout: 5) do |g|
+          g.command TRAP_SIGNAL
+          g.command "fail", abort_on_error: true
         end
         pg = cg.run(parallel: true)
         pg.wait
