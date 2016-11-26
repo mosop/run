d06481113f5bd16309e118244286b21a180a2ff2
mosop <mosop@users.noreply.github.com>
2016-11-26 21:19:40 +0900

    fix spec

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -3,4 +3,5 @@ require "stdio"
 require "crystal_plus/dir/.tmp"
 require "../src/run"
 
-TRAP_SIGNAL = File.expand_path("#{__DIR__}/../bin/trap-signal")
+# TRAP_SIGNAL = File.expand_path("#{__DIR__}/../bin/trap-signal")
+TRAP_SIGNAL = File.expand_path("#{__DIR__}/../scripts/trap-signal")
